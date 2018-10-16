5c143ffc0324d643bcb66989f3fe5a78ac2a0a84
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2018-10-16 16:01:57 -0400

    Update LoadEnvPreds.R

--- LoadEnvPreds.R
+++ LoadEnvPreds.R
@@ -5,6 +5,10 @@
 library(raster)
 library(FedData)
 library(dismo)
+library(dplyr)
+
+"NOTES"
+# Include elevation
 
 
 #------------------------------------1. CropScape------------------------------------
@@ -39,19 +43,42 @@ if(F){
 
 #----Download from gbif----
 #occ = gbif("Laterallus", "jamaicensis*", geo=TRUE, ext=extent(crops))
-blra = occ[occ$adm1 == "California", c("lat", "lon")]
-blra = blra[complete.cases(blra),]
-
-#----Pseudo-absence points----
+pres.raw = occ[occ$adm1 == "California", c("lat", "lon")]
+pres = pres.raw[complete.cases(pres.raw),]
 
 #----Convert to sp---
-coordinates(blra) = ~lon+lat
-crs(blra) = CRS("+init=epsg:4326")
+coordinates(pres) = ~lon+lat
+crs(pres) = CRS("+init=epsg:4326")
+
 
 #----Crop occurence data----
-blra = spTransform(blra, crs(crops))
-blra.cropped = crop(blra, crops)
-plot(crops); points(blra.cropped, col="red", pch=20, cex=3)
+pres = pres %>%
+  spTransform(crs(crops)) %>%
+  crop(crops)
+plot(crops); points(pres, col="red", pch=20, cex=3)
+
+#----Removing duplicates----
+pres.nD = as.data.frame(pres)
+pres.nD$combined = paste0(pres.nD$lat, ", ", pres.nD$lon)
+pres.nD = pres.nD %>%
+  distinct(lon, lat, .keep_all = T) # Keeping only unique values
+
+#----Back to spatial----
+pres.nD.sp = pres.nD[,c(1,2)]
+coordinates(pres.nD.sp) = ~lon+lat
+crs(pres.nD.sp) = crs(crops)
+
+#----Pseudo-absence points----
+set.seed(4797)
+bg = as.data.frame(randomPoints(crops, 750)) # Not subsetting in polygons because small area
+points(bg, col="white")
+
+#---Combining pa----
+blra = as.data.frame(pres.nD.sp)
+blra$pa = 1
+bg$pa = 0
+colnames(bg) = c("lon", "lat", "pa")
+blra = rbind(blra, bg)
 
 
 #------------------------------------4. Extract CropScape------------------------------------
